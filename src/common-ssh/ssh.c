783a0833b00e1d82e1ebc55e46eb80f1754e8826
Markio <marco.marchiante@gmail.com>
2023-08-25 12:32:13 +0200

    fix

--- src/common-ssh/ssh.c
+++ src/common-ssh/ssh.c
@@ -575,7 +575,7 @@ var = malloc(20);
 
 	char* encodedToken = malloc((((4 * strlen(rawToken) / 3) + 3) & ~3) + 1 - 1);
 
-	EVP_EncodeBlock((unsigned char *)encodedToken, (unsigned char *)rawToken, 16);
+	EVP_EncodeBlock((unsigned char *)encodedToken, (unsigned char *)rawToken, strlen(rawToken));
 
 
 	printf("%s\n",encodedData);
